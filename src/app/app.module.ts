3587c6a1d10c531fe25a9acd0843b3b454fd5108
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2023-06-29 15:27:30 +0200

    added product navigation via anchor links in main page to products page. added product detail page logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,10 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    RouterModule.forRoot([
+    RouterModule.forRoot
+    ([
       { path: '', component: ProductListComponent },
+      { path: 'products/:productId', component: ProductDetailsComponent },
     ])
   ],
   declarations: [
