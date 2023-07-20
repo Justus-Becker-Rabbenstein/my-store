580ca6ade91ad23a783499968918d8fac78c3a65
Justus-Becker-Rabbenstein <43116175+Justus-Becker-Rabbenstein@users.noreply.github.com>
2023-07-20 11:46:30 +0200

    started implementation of api fetch of shipping.json. implementation not complete.

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
-/* . . . */
+
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
   items: Product[] = [];
-/* . . . */
 
   addToCart(product: Product) {
     this.items.push(product);
@@ -20,5 +25,9 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-/* . . . */
+
+  getShippingPrices() {
+    return this.http.get<{ type: string, price: number }[]>('/assets/shipping.json');
+  }
+
 }
